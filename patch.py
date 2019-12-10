56b378b841fc53525b25e41b87aeb54616361260
Rob Nee <robnee@users.noreply.github.com>
2019-12-10 07:47:26 -0500

    add mapping for task functions

--- patch.py
+++ patch.py
@@ -10,23 +10,29 @@ def patch():
     def run(task, debug=False):
         try:
             loop = asyncio.get_event_loop()
-        except:
+        except Exception:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-
+            
         if debug:
             loop.set_debug(True)
-            logging.getLogger("asyncio").setLevel(logging.DEBUG)
-            warnings.filterwarnings("always")
+            logging.getLogger('asyncio').setLevel(logging.DEBUG)
+            warnings.filterwarnings('always')
         else:
             loop.set_debug(False)
-            logging.getLogger("asyncio").setLevel(logging.WARNING)
-            warnings.filterwarnings("default")
-
-        return loop.run_until_complete(task)
+            logging.getLogger('asyncio').setLevel(logging.WARNING)
+            warnings.filterwarnings('default')
+            
+        response = loop.run_until_complete(task)
+        
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        
+        return response
 
     version = sys.version_info.major * 10 + sys.version_info.minor
     if version < 37:
         asyncio.get_running_loop = asyncio.get_event_loop
         asyncio.create_task = asyncio.ensure_future
+        asyncio.current_task = asyncio.Task.current_task
+        asyncio.all_tasks = asyncio.Task.all_tasks
         asyncio.run = run
